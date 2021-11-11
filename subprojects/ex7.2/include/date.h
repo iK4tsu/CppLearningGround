ca5b28828c444e6744da8772b3ec69d16ccfcaee
João Lourenço <jlourenco5691@gmail.com>
2021-11-11 20:29:46 +0000

    subprojects/ex7.2/include: implement date definition
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ subprojects/ex7.2/include/date.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class Date
+{
+public:
+	constexpr Date() = default;
+	constexpr Date(int day, int month, int year)
+		: m_day(day)
+		, m_month(month)
+		, m_year(year)
+	{}
+
+	~Date() = default;
+	constexpr Date(const Date& other) = default;
+
+	constexpr int day() const { return m_day; }
+	constexpr int month() const { return m_month; }
+	constexpr int year() const { return m_year; }
+
+private:
+	int m_day{1};
+	int m_month{0};
+	int m_year{1970};
+};
