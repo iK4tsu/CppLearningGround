f6cf435c1305d58bcfb8be17908398abf2fa6240
João Lourenço <jlourenco5691@gmail.com>
2021-11-12 00:52:14 +0000

    subprojects/ex7.2/source/main: implement required program
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- subprojects/ex7.2/source/main.cpp
+++ subprojects/ex7.2/source/main.cpp
@@ -1,4 +1,32 @@
+#include <iostream>
+#include <vector>
+
+#include "student.h"
+#include "fellow.h"
+
 int main()
 {
+	std::vector<Student> students;
+	students.push_back(Student("Maria", 9855678, Date(18, 7, 1990), Date(1, 9, 2014)));
+	students.push_back(Student("Maria", 9855678, Date(18, 7, 1990)));
+
+	std::vector<Fellow> fellows;
+	fellows.push_back(Fellow("Maria", 9855678, Date(18, 7, 1990), Date(1, 9, 2014), 1500.5));
+	fellows.push_back(Fellow("Maria", 9855678, Date(18, 7, 1990)));
+
+	Student s;
+	std::cout << "Info for a new student:" << std::endl;
+	std::cin >> s;
+	students.push_back(s);
+
+	Fellow f;
+	std::cout << "Info for a new fellow:" << std::endl;
+	std::cin >> f;
+	fellows.push_back(f);
+
+	for(const auto& x : students) std::cout << x << '\n';
+	for(const auto& x : fellows) std::cout << x << '\n';
+	std::cout.flush();
+
 	return 0;
 }
