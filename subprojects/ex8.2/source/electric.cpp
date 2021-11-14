00928ff7c8f23c68398ebb325fb73700a9e3f6f3
João Lourenço <jlourenco5691@gmail.com>
2021-11-14 22:28:31 +0000

    subprojects/ex8.2/include/electric.h: implement extraction operator overload
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- subprojects/ex8.2/source/electric.cpp
+++ subprojects/ex8.2/source/electric.cpp
@@ -3,3 +3,12 @@
 Electric::Electric(double batteryLife)
 	: m_batteryLife(batteryLife)
 {}
+
+std::istream& operator>>(std::istream& istream, Electric& self)
+{
+	istream >> static_cast<Vehicle&>(self);
+	istream >> self.m_wheels;
+	istream >> self.m_batteryLife;
+
+	return istream;
+}
