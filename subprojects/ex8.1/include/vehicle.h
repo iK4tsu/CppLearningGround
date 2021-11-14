39497d8705670433a1f6608b8819ddbfe862eba3
João Lourenço <jlourenco5691@gmail.com>
2021-11-14 12:34:45 +0000

    subprojects/ex8.1/include: implement vehicle base structure
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ subprojects/ex8.1/include/vehicle.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string_view>
+
+class Vehicle
+{
+public:
+	enum class Color { red, yellow, green, unknown };
+
+	constexpr static std::string_view strFromColor(const Color& color)
+	{
+		switch(color)
+		{
+			using enum Color;
+			case red:     return "red";
+			case yellow:  return "yellow";
+			case green:   return "green";
+			case unknown:
+			default:      return "";
+		}
+	}
+
+private:
+	static size_t m_globalSerial;
+
+protected:
+	Vehicle(unsigned short year, Color color, unsigned short wheels);
+	Vehicle() = default;
+	virtual ~Vehicle() = default;
+
+	unsigned short m_year{2'000};
+	Color m_color{Color::unknown};
+	unsigned short m_wheels{4};
+	size_t m_serialNumber{m_globalSerial++};
+};
