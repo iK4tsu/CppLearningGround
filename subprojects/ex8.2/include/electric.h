53d8442ad4afc4a1f7a6e94a1fabed7c36745fbf
João Lourenço <jlourenco5691@gmail.com>
2021-11-14 22:58:21 +0000

    subprojects/ex8.2/include/electric.h: implement toSStringParams method
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- subprojects/ex8.2/include/electric.h
+++ subprojects/ex8.2/include/electric.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <istream>
+#include <sstream>
 #include "vehicle.h"
 
 class Electric : public virtual Vehicle
@@ -14,6 +15,13 @@ public:
 
 protected:
 	Electric(double batteryLife);
+
+	inline std::stringstream toSStringParams() const override
+	{
+		std::stringstream sstream{Vehicle::toSStringParams()};
+		sstream << ", " << m_batteryLife;
+		return sstream;
+	}
 
 	double m_batteryLife{};
 };
