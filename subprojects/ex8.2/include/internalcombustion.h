a9eb26bf2fcc167333754cb6bd511a51537abe17
João Lourenço <jlourenco5691@gmail.com>
2021-11-14 22:14:16 +0000

    subprojects/ex8.2/include/internalcombustion.h: fix default constructor without public visibility
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- subprojects/ex8.2/include/internalcombustion.h
+++ subprojects/ex8.2/include/internalcombustion.h
@@ -5,13 +5,14 @@
 class InternalCombustion : public virtual Vehicle
 {
 public:
+	InternalCombustion() = default;
+
 	virtual inline double emissionCO2() const
 	{
 		return m_cylinderVol * 0.1;
 	}
 
 protected:
-	InternalCombustion() = default;
 	InternalCombustion(double cylinderVol);
 
 	double m_cylinderVol{};
