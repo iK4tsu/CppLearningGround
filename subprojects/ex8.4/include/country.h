938a171996c5f40e88d68b1459596670752fd127
João Lourenço <jlourenco5691@gmail.com>
2021-11-16 20:40:46 +0000

    subprojects/ex8.4/include: implement country base structure
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ subprojects/ex8.4/include/country.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <set>
+#include "city.h"
+#include "district.h"
+
+class Country
+{
+public:
+	Country() = default;
+
+	template<class T1, class T2>
+	Country(T1&& name, T2&& capital)
+		: m_name{std::forward<T1>(name)}
+		, m_capital(std::forward<T2>(capital))
+	{}
+
+private:
+	std::string m_name{};
+	City m_capital{};
+	std::set<District> m_districts{};
+};
