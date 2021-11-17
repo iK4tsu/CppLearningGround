b86fc4cf2e13a770b5b08201735860fb26f8f51c
João Lourenço <jlourenco5691@gmail.com>
2021-11-17 22:25:59 +0000

    subprojects/ex8.challenge/include/user.h: implement User base structure
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ subprojects/ex8.challenge/include/user.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+class User
+{
+public:
+	User() = default;
+
+	template<class T1, class T2, class T3>
+	User(T1&& name, T2&& email, T3&& mobile)
+		: m_name(std::forward<T1>(name))
+		, m_email(std::forward<T2>(email))
+		, m_mobile(std::forward<T3>(mobile))
+	{}
+
+	inline const std::string& name() const { return m_name; }
+	inline const std::string& email() const { return m_email; }
+	inline const std::string& mobile() const { return m_mobile; }
+
+	inline void name(std::string&& name) { m_name = std::move(name); }
+	inline void email(std::string&& email) { m_email = std::move(email); }
+	inline void mobile(std::string&& mobile) { m_mobile = std::move(mobile); }
+
+private:
+	std::string m_name{};
+	std::string m_email{};
+	std::string m_mobile{};
+};
