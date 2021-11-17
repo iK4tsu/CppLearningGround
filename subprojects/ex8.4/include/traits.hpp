4a0137144fc4408dbd692bba80aa075857b79646
João Lourenço <jlourenco5691@gmail.com>
2021-11-17 07:51:01 +0000

    subprojects/ex8.4/include/traits.hpp: add is_convertible_unqual trait
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- subprojects/ex8.4/include/traits.hpp
+++ subprojects/ex8.4/include/traits.hpp
@@ -10,3 +10,12 @@
 */
 template<typename T1, typename T2>
 inline constexpr bool is_same_unqual = std::is_same_v<std::remove_cvref_t<T1>, T2>;
+
+/**
+@brief Checks if T1 with all qualifiers removed, if any, is convertible to T2's type
+@param T1 actual type
+@param T2 expected type
+@returns std::true_type if T1 is convertible to T2, std::false_type otherwise
+*/
+template<typename T1, typename T2>
+inline constexpr bool is_convertible_unqual = std::is_convertible_v<std::remove_cvref_t<T1>, T2>;
